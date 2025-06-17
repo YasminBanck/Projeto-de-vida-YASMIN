b145984c0c54cc02c57d262cd770158a842ad5be
yasmin Banck <145784815+YasminBanck@users.noreply.github.com>
2025-06-17 07:42:06 -0300

    tete 0.1

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-05T14:00:00");
-const tempoObjetivo2 = new Date("2025-06-26T15:00:00");
+const tempoObjetivo1 = new Date("2026-02-15T14:00:00");
+const tempoObjetivo2 = new Date("2025-10-19T15:00:00");
 const tempoObjetivo3 = new Date("2025-12-20T20:00:00");
 const tempoObjetivo4 = new Date("2025-11-30T14:00:00");
 
